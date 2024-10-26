09e28c1a7148ceeefeecc2bb45cff29c488d8ad1
henryz2004 <henryz2004@gmail.com>
2024-10-26 13:35:40 -0400

    hides api keys

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-# Import the Python SDK
+import os
+from dotenv import load_dotenv, find_dotenv, dotenv_values
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyBv2e8LO9VwnBgAIYcSEJNLsMy2LyKucT4")
+# Load environment variables from .env file
+load_dotenv(find_dotenv())
+
+# Fetch the GOOGLE_API_KEY
+api_key = os.getenv('GOOGLE_API_KEY')
+
+genai.configure(api_key=api_key)
 model = genai.GenerativeModel('gemini-1.5-flash')
 
 def generate_prompts(user_input):
-  response = model.generate_content(f"Give me 100 general prompts about a {user_input} for a large language model. Only give to me in a python list and do not include any other text.")
+  response = model.generate_content(f"Give me 20 general prompts about a {user_input} for a large language model. Only give to me in a python list and do not include any other text.")
   return response.text
+
+print(generate_prompts("zebra"))
