8c90279dead63aa4b4956df66393e48329b9bbbb
henryz2004 <henryz2004@gmail.com>
2024-10-26 16:52:42 -0400

    adds function to sample mean activation for a model and set of prompts at the layer level

--- prompt_gen.py
+++ prompt_gen.py
@@ -14,5 +14,3 @@ model = genai.GenerativeModel('gemini-1.5-flash')
 def generate_prompts(user_input):
   response = model.generate_content(f"Create 100 generic prompts for an llm. Include the following topic in each prompt: {user_input}. Write it as a list of prompts only separated by newlines, and include no other text.")
   return response.text.split("\n")
-
-print(generate_prompts("zebra"))
