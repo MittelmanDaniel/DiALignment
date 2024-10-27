a8db3bc022247f901c4428767758680e4aa767e5
Kevinouio <133690459+Kevinouio@users.noreply.github.com>
2024-10-27 07:21:53 -0400

    Final Web app

--- webapp.py
+++ webapp.py
@@ -16,6 +16,7 @@ import requests
 import io
 import textwrap
 import gc
+import json
 import qwen_refusal as qr
 import requests
 from datasets import load_dataset
@@ -28,12 +29,13 @@ from transformer_lens.hook_points import HookPoint
 from transformers import AutoTokenizer
 from jaxtyping import Float, Int
 from colorama import Fore
+import asyncio
+import aiohttp
 
 # Configure the Google generative model
 # Add API KEYS
 gemini_api_key = os.getenv("GEMINI_API_KEY")
 anthropic_api_key = os.getenv("ANTHROPIC_API_KEY")
-
 
 genai.configure(api_key=gemini_api_key)
 gemini_model = genai.GenerativeModel('gemini-1.5-flash')
@@ -68,68 +70,38 @@ def generate_with_gemini(prompt):
     return clean_text
 
 
-def generate_llama(prompt):
-    print("hello world")
+# Gets the generation response from another computer that has the llama
+async def gen_llama(prompt,secondary_prompt="",overall_prompt="", intensity=0):
+    async with aiohttp.ClientSession() as session:
+        if secondary_prompt != "":
+            topics = [ secondary_prompt, overall_prompt]
+            async with session.post("http://d390-143-215-61-120.ngrok-free.app/ablate/llama",
+                                    data=json.dumps({"message": prompt, "topics": topics, "amplitude": intensity})) as response:
+                return await response.json()
+        else:
+            async with session.post("http://d390-143-215-61-120.ngrok-free.app/ablate/llama",
+                                    data=json.dumps({"message": f"{prompt}", "amplitude": intensity})) as response:
+                return await response.json()
 
-
-def generate_qwen(prompt):
-    # Define the URL
-    url = "https://dc2f-143-215-61-120.ngrok-free.app/test"
-    print("HERE1")
-    # Define the payload
-    data = {
-        "message": f"{prompt}",
-        "amplitude": 1  # Example amplitude value
-    }
-    print("HERE2")
-    # Send the POST request
-    response = requests.post(url, json=data)
-    print("HERE")
-    # Check if the request was successful
-    if response.status_code == 200:
-        # Parse the JSON response
-        result = response.json()
-        print("Baseline Generations:", result["baseline"])
-        print("Intervention Generations:", result["intervention"])
-        return result
-    else:
-        print(f"Request failed with status code {response.status_code}")
-        print("Response:", response.text)
-
-
-# Define function to generate prompts using Claude model
-def generate_with_claude(prompt, intensity=0):
-    prompt_message = f"Generate a list of 30 prompts about {prompt} suitable for a large language model. Only give the response in a Python list format and do not include any other text."
-    print("HERE")
-    response = client.messages.create(
-        model="claude-3-haiku-20240307",
-        max_tokens=1000,
-        messages=[
-            {"role": "user", "content": f"{prompt_message}"}
-        ]
-    )
-    print(response.content[0].text)
-    prompts = ast.literal_eval(response.content[0].text)
-
-    # Check if it's a list
-    if isinstance(prompts, list):
-        # Access individual strings
-        for i, prompt in enumerate(prompts):
-            print(f"Prompt {i + 1}: {prompt}")
-    else:
-        print("The content is not a list.")
-
-    return prompts
+# Gets the generation response from another computer that has the Gwen
+async def gen_qwen(prompt, intensity=0):
+    async with aiohttp.ClientSession() as session:
+        async with session.post("http://d390-143-215-61-120.ngrok-free.app/ablate/qwen",
+                                data=json.dumps({"message": f"{prompt}", "amplitude": intensity})) as response:
+            return await response.json()
 
 
 # Generate responses based on the chosen model
-def generate_response(prompt, model, intensity=5):
-    if model == "Gemini":
-        return generate_with_gemini(prompt)
-    elif model == "Claude":
-        return generate_with_claude(prompt, intensity)
+async def generate_response(prompt, secondary_prompt="",overall_prompt="",  model="Llama", intensity=0):
+    if secondary_prompt != "":
+        res = await gen_llama(prompt, secondary_prompt, overall_prompt, intensity)
+        return res
+    if model == "Llama":
+        res = await gen_llama(prompt, intensity=intensity)
+        return res
     elif model == "Qwen":
-        return generate_qwen(prompt)
+        res = await gen_qwen(prompt, intensity)
+        return res
 
 
 # Streamed response emulator
@@ -137,26 +109,6 @@ def response_display(response):
     for word in response.split():
         yield word + " "
         time.sleep(0.05)
-
-
-def evaluate_harmfulness(model_names, harm_labels):
-    # Define the mapping for string values
-    harm_mapping = {"benign": 0, "somewhat harmful": 0.5, "extremely harmful": 1}
-
-    # Prepare list to store average scores
-    scores = []
-
-    # Iterate over each model and its corresponding list of labels
-    for labels in harm_labels:
-        # Map each label to its numeric value and calculate the average
-        mapped_values = [harm_mapping[label] for label in labels]
-        avg_score = sum(mapped_values) / len(mapped_values)
-        scores.append(avg_score)
-
-    # Create the DataFrame with model names and their average scores
-    df = pd.DataFrame({"Model Name": model_names, "Harmfulness Score": scores})
-
-    return df
 
 # Title and description
 st.title("DiALignment")
@@ -169,10 +121,22 @@ with c1:
 with c2:
     intensity = st.slider("Choose the intensity level (0-10):", -1.0, 1.0, 0.0)
 
+# Create a checkbox for enabling the secondary input
+enable_secondary_prompt = st.checkbox("Enable Albilation prompt?")
+
+# Display a secondary prompt input if the checkbox is enabled
+secondary_prompt = ""
+if enable_secondary_prompt:
+    colu1, colu2 = st.columns(2)
+    with colu1:
+        secondary_prompt = st.text_input("Enter your secondary prompt:", "Explain the impact of AI on society.")
+    with colu1:
+        overall_prompt = st.text_input("Enter your overall prompt:", "Explain the impact of AI on society.")
+
+
+
 # Model selection dropdown
-model_choice = st.selectbox("Choose a model to generate prompts:", ("Gemini", "Claude", "Llama", "Qwen"))
-
-
+model_choice = st.selectbox("Choose a model to generate prompts:", ("Llama", "Qwen"))
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -180,72 +144,57 @@ if "messages" not in st.session_state:
 
 # Button to send the initial prompt to the chatbot
 if st.button("Generate Responses"):
+
     with st.spinner("Generating..."):
-
-        url = "https://dc2f-143-215-61-120.ngrok-free.app/test"
-        print("HERE1")
-        # Define the payload
-        data = {
-            "message": f"ererew",
-            "amplitude": 1  # Example amplitude value
-        }
-        print("HERE2")
-        # Send the POST request
-        response = requests.get(url, json=data)
-        print("HERE")
-        # Check if the request was successful
-        if response.status_code == 200:
-            # Parse the JSON response
-            result = response.json()
-            print("Baseline Generations:", result["baseline"])
-            print("Intervention Generations:", result["intervention"])
-
-        else:
-            print(f"Request failed with status code {response.status_code}")
-            print("Response:", response.text)
-
         # Generate responses for the normal and modified intensity values
-        #normal_prompts = generate_response(init_prompt, model_choice, intensity=0)
-        #modified_prompts = generate_response(init_prompt, model_choice, intensity=intensity)
+        if enable_secondary_prompt:
+            modified_prompts = asyncio.run(generate_response(init_prompt, secondary_prompt, overall_prompt, model=model_choice, intensity=intensity))
+        else:
+            modified_prompts = asyncio.run(generate_response(init_prompt, model=model_choice, intensity=intensity))
+
+        qprompt1 = modified_prompts["baseline"]
+        qprompt2 = modified_prompts["intervention"]
 
     # Chat window style output
     col1, col2 = st.columns(2)
 
     with col1:
         st.subheader("Normal Model Response")
-        normal_response_content = "".join(response_display(normal_prompts[0]))
+
+        if model_choice == "Llama":
+            normal_response_content = qprompt1
+        else:
+            normal_response_content = qprompt1[0]
+
         st.markdown(normal_response_content)
 
     with col2:
         st.subheader("Modified Model Response")
-        modified_response_content = "".join(response_display(modified_prompts[0]))
+        if model_choice == "Llama":
+            modified_response_content = qprompt2
+        else:
+            modified_response_content = qprompt2[0]
         st.markdown(modified_response_content)
 
-    '''
-    # After normal generation
-    responses = []
-    for models in model_names:
-        responses.append(generate_response(init_prompt, models, intensity=0))
-    evals = get_ratings(responses)
+# Replace this with the pickle that is made for the leaderboard metrics
+data = pd.read_pickle("general-qwen.pkl")
 
-    df = evaluate_harmfulness(model_names, evals)
+df = pd.DataFrame(data)
 
+st.dataframe(df)
 
-    st.dataframe(df)
+sb_options = list(df.columns)
+sb_options.remove('model_names')
 
-    sb_options = list(df.columns)
-    sb_options.remove('Name')
+colselect1, colselect2 = st.columns(2)
+with colselect1:
+    sel_metric = st.selectbox('Select the Metric', options=sb_options)
 
-    colselect1, colselect2 = st.columns(2)
-    with colselect1:
-        sel_metric = st.selectbox('Select the Metric', options=sb_options)
+# Create a new dataframe for plotting. Sort it for plotting display.
+# If metric value is the same, display name by alphabetic order.
+df_metric = df.copy()
+df_metric = df_metric.sort_values(by=[sel_metric, 'model_names'], ascending=[True, False])
 
-    # Create a new dataframe for plotting. Sort it for plotting display.
-    # If metric value is the same, display name by alphabetic order.
-    df_metric = df.copy()
-    df_metric = df_metric.sort_values(by=[sel_metric, 'Name'], ascending=[True, False])
-
-    plot_assym = px.bar(df_metric, x=sel_metric, y="Name", template="plotly_white", text_auto=True)
-    st.plotly_chart(plot_assym)
-    '''
+plot_assym = px.bar(df_metric, x=sel_metric, y="model_names", template="plotly_white", text_auto=True)
+st.plotly_chart(plot_assym)
 
